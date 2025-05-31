029316a4a866c22f83f928274f1795e48c801225
Amadou-Salifou <amadousalif412@gmail.com>
2025-05-31 02:57:57 +0200

    Ajout de l'authentification Firebase

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDH3Hp_2eyFGZBbUgIBkc65ua48lz-aqmU",
+    authDomain: "uam-mines-et-petrole.firebaseapp.com",
+    projectId: "uam-mines-et-petrole",
+    storageBucket: "uam-mines-et-petrole.firebasestorage.app",
+    messageingSenderId:"896440858732",
+    appId: "1:896440858732:web:73cc57d6d482a9828f0049",
+};
+
+firebase.initializeApp(firebaseConfig);
